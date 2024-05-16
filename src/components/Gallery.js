f1eb4c317a39f1fac902f85bb677bbd2e496eed7
robertoparet <92403963+robertoparet@users.noreply.github.com>
2024-05-16 23:33:57 +0200

    correccion imagenes duplicadas

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -25,13 +25,14 @@ import img4 from '../assets/px04.jpg';
 import img5 from '../assets/px05.jpg';
 import img6 from '../assets/px06.jpg';
 import img7 from '../assets/px07.jpg';
-import img8 from '../assets/px08.jpg';
 import img9 from '../assets/px09.jpg';
 import img11 from '../assets/px11.jpg';
 import img12 from '../assets/px12.jpg';
 import img13 from '../assets/px13.jpg';
 import img14 from '../assets/px14.jpg';
 import img15 from '../assets/px15.jpg';
+import img16 from '../assets/px16.jpg';
+import img17 from '../assets/px17.jpg';
 import img18 from '../assets/px18.jpg';
 import img19 from '../assets/px19.jpg';
 import img20 from '../assets/px20.jpg';
@@ -40,10 +41,12 @@ import img22 from '../assets/px22.jpg';
 import img23 from '../assets/px23.jpg';
 import img24 from '../assets/px24.jpg';
 import img25 from '../assets/px25.jpg';
+import img26 from '../assets/px26.jpg';
 import img27 from '../assets/px27.jpg';
 import img28 from '../assets/px28.jpg';
 import img29 from '../assets/px29.jpg';
 import img30 from '../assets/px30.jpg';
+import img31 from '../assets/px31.jpg';
 import img32 from '../assets/px32.jpg';
 import img33 from '../assets/px33.jpg';
 import img38 from '../assets/px38.jpg';
@@ -56,7 +59,6 @@ import img45 from '../assets/px45.jpg';
 import img46 from '../assets/px46.jpg';
 import img47 from '../assets/px47.jpg';
 
-
 const images = [
     { src: img1, alt: "1" },
     { src: img2, alt: "2" },
@@ -65,13 +67,14 @@ const images = [
     { src: img5, alt: "5" },
     { src: img6, alt: "6" },
     { src: img7, alt: "7" },
-    { src: img8, alt: "8" },
     { src: img9, alt: "9" },
     { src: img11, alt: "11" },
     { src: img12, alt: "12" },
     { src: img13, alt: "13" },
     { src: img14, alt: "14" },
     { src: img15, alt: "15" },
+    { src: img16, alt: "16" },
+    { src: img17, alt: "17" },
     { src: img18, alt: "18" },
     { src: img19, alt: "19" },
     { src: img20, alt: "20" },
@@ -80,10 +83,12 @@ const images = [
     { src: img23, alt: "23" },
     { src: img24, alt: "24" },
     { src: img25, alt: "25" },
+    { src: img26, alt: "26" },
     { src: img27, alt: "27" },
     { src: img28, alt: "28" },
     { src: img29, alt: "29" },
     { src: img30, alt: "30" },
+    { src: img31, alt: "31" },
     { src: img32, alt: "32" },
     { src: img33, alt: "33" },
     { src: img38, alt: "38" },
@@ -96,6 +101,7 @@ const images = [
     { src: img46, alt: "46" },
     { src: img47, alt: "47" }
 ];
+
 
 
 
